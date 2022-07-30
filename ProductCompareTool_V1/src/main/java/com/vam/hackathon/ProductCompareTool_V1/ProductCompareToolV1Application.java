ac18ce53caeab6aca81d3e727e32b187db0ad9d1
madhuri-korla <110281854+madhuri-korla@users.noreply.github.com>
2022-07-30 10:17:14 +0000

    initial set up of project, added 15 files

---
+++ ProductCompareTool_V1/src/main/java/com/vam/hackathon/ProductCompareTool_V1/ProductCompareToolV1Application.java
@@ -0,0 +1,13 @@
+package com.vam.hackathon.ProductCompareTool_V1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCompareToolV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCompareToolV1Application.class, args);
+	}
+
+}
---
+++ ProductCompareTool_V1/src/test/java/com/vam/hackathon/ProductCompareTool_V1/ProductCompareToolV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vam.hackathon.ProductCompareTool_V1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompareToolV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
